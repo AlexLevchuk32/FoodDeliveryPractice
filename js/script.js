e7c2f7860adb07ad46e9ce8c057072b59ec77d8a
Alex Levchuk <alexlevchuk@mail.ru>
2023-06-22 19:13:22 +0300

    added tabs js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const tabs = document.querySelectorAll('.tabheader__item');
+	const tabsContent = document.querySelectorAll('.tabcontent');
+	const tabsParrent = document.querySelector('.tabheader');
+
+	function hideTabContent() {
+		tabsContent.forEach((item) => {
+			item.classList.add('hide');
+			item.classList.remove('show', 'fade');
+		});
+
+		tabs.forEach((item) => {
+			item.classList.remove('tabheader__item_active');
+		});
+	}
+
+	// Присваиваем аргумент по умолчанию, т.е. показываем первый элемент из псевдомассива
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add('show', 'fade');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	tabsParrent.addEventListener('click', (event) => {
+		const target = event.target;
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+});
