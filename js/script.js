bbe6f0d3b773f24d7a3aa17b4f193c0d2d32887f
Alex Levchuk <alexlevchuk@mail.ru>
2023-06-27 20:14:35 +0300

    added timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
+	// ==================================================================================================================================================================================================================
+	// Табы
+
 	const tabs = document.querySelectorAll('.tabheader__item');
 	const tabsContent = document.querySelectorAll('.tabcontent');
 	const tabsParrent = document.querySelector('.tabheader');
@@ -36,4 +39,70 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+
+	// ==================================================================================================================================================================================================================
+	// Таймер для акции
+
+	// Устанавливаем даут окончания акции
+	const deadline = '2023-08-31';
+
+	// Опеределяем разниуц между окончанием акции и текущим временем
+	function getTimeRemaining(endtime) {
+		// Получаем количество миллисекунд
+		const time = Date.parse(endtime) - Date.parse(new Date());
+		// Получаем количество дней
+		const days = Math.floor(time / (1000 * 60 * 60 * 24));
+		// Получаем количество часов
+		const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+		// Получаем количество минут
+		const minutes = Math.floor((time / 1000 / 60) % 60);
+		// Получаем количество секунд
+		const seconds = Math.floor((time / 1000) % 60);
+
+		return {
+			total: time,
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds,
+		};
+	}
+
+	// Подставляем ноль перед единичным символом в ячейче
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	// Устанавливаем таймер на страницу
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector);
+		const days = timer.querySelector('#days');
+		const hours = timer.querySelector('#hours');
+		const minutes = timer.querySelector('#minutes');
+		const seconds = timer.querySelector('#seconds');
+		const timeInterval = setInterval(updateClock, 1000);
+
+		// Убираем мигание верстки при обновлении страницы
+		updateClock();
+
+		// Обновляем таймер каждую секунду
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock('.timer', deadline);
 });
